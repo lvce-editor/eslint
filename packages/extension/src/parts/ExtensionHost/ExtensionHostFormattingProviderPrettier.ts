1c3ed29401d00a547e31375818a185e2cbe84205
Le Vivilet <levivilet@pm.me>
2025-11-27 16:54:02 +0100

    fix

--- packages/extension/src/parts/ExtensionHost/ExtensionHostFormattingProviderPrettier.ts
+++ packages/extension/src/parts/ExtensionHost/ExtensionHostFormattingProviderPrettier.ts
@@ -1,5 +1,3 @@
-import * as Format from '../Format/Format.ts'
-
 export const id = 'prettier'
 
 export const label = 'Prettier'
@@ -7,21 +5,5 @@ export const label = 'Prettier'
 export const languageId = 'css'
 
 export const format = async (textDocument) => {
-  const uri = textDocument.uri
-  // @ts-expect-error
-  const text = vscode.getTextFromTextDocument(textDocument)
-  const start = performance.now()
-  const minimizedEdit = await Format.format(uri, text)
-  const end = performance.now()
-  console.log('took', end - start, 'ms')
-  if (!minimizedEdit) {
-    return []
-  }
-  return [
-    {
-      startOffset: minimizedEdit.startOffset,
-      endOffset: minimizedEdit.endOffset,
-      inserted: minimizedEdit.inserted,
-    },
-  ]
+  return []
 }
