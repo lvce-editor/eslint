7e00b2b191cf4edcbd480e7b709613d57c73d793
Le Vivilet <levivilet@pm.me>
2025-11-27 16:56:58 +0100

    feature: add initial files (#1)

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
-# eslint
+# ESLint
+
+## Contributing
+
+```sh
+git clone git@github.com:lvce-editor/eslint.git &&
+cd eslint &&
+npm ci &&
+npm test
+```
---
+++ packages/extension/src/parts/ExtensionHost/ExtensionHostFormattingProviderPrettier.ts
@@ -0,0 +1,9 @@
+export const id = 'prettier'
+
+export const label = 'Prettier'
+
+export const languageId = 'css'
+
+export const format = async (textDocument) => {
+  return []
+}
