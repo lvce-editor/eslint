f4f4dace240f78cce58803e7b4324f82909985e6
Le Vivilet <levivilet@pm.me>
2025-11-27 17:31:33 +0100

    feature: add diagnostics e2e test (#7)

---
+++ packages/e2e/src/eslint.validate-javascript.ts
@@ -0,0 +1,32 @@
+import type { Test } from '@lvce-editor/test-with-playwright'
+
+export const name = 'eslint.validate-javascript'
+
+export const skip = 1
+
+export const test: Test = async ({
+  FileSystem,
+  Main,
+  Editor,
+  Locator,
+  expect,
+}) => {
+  // arrange
+  const tmpDir = await FileSystem.getTmpDir()
+  await FileSystem.writeFile(
+    `${tmpDir}/test.js`,
+    `let x = '1'
+
+x++`,
+  )
+  await Main.openUri(`${tmpDir}/test.js`)
+
+  // act
+  // await Editor.format()
+
+  // // assert
+  // const editor = Locator('.Editor')
+  // await expect(editor).toHaveText(`h1 {  font-size: 10px;}`)
+
+  // TODO check for diagnostics
+}
