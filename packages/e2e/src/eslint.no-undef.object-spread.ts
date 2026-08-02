1d62b9dfb1140af983739ad558cb69d0b364bb4c
Le Vivilet <levivilet@pm.me>
2026-08-02 01:01:55 -0700

    feature: add 120 eslint e2e tests (#35)

---
+++ packages/e2e/src/eslint.no-undef.object-spread.ts
@@ -0,0 +1,28 @@
+import type { Test } from '@lvce-editor/test-with-playwright'
+
+export const name = 'eslint.no-undef.object-spread'
+
+const expectedDiagnostics = [{ source: 'no-undef', type: 'error' }]
+
+export const test: Test = async ({ Command, FileSystem, Main, Workspace }) => {
+  const tmpDir = await FileSystem.getTmpDir()
+  const uri = `${tmpDir}/test.js`
+  const text = 'export default { ...missing }'
+  await FileSystem.writeFile(uri, text)
+  await Workspace.setPath(tmpDir)
+  await Main.openUri(uri)
+
+  const { result: diagnostics } = (await Command.executeExtensionCommand(
+    'eslint.lint',
+    { text, uri },
+  )) as { result: readonly { source: string; type: string }[] }
+  const actualDiagnostics = diagnostics.map(({ source, type }) => ({
+    source,
+    type,
+  }))
+  if (
+    JSON.stringify(actualDiagnostics) !== JSON.stringify(expectedDiagnostics)
+  ) {
+    throw new Error(`Unexpected diagnostics: ${JSON.stringify(diagnostics)}`)
+  }
+}
---
+++ packages/e2e/src/eslint.no-unreachable.getter-return.ts
@@ -0,0 +1,29 @@
+import type { Test } from '@lvce-editor/test-with-playwright'
+
+export const name = 'eslint.no-unreachable.getter-return'
+
+const expectedDiagnostics = [{ source: 'no-unreachable', type: 'error' }]
+
+export const test: Test = async ({ Command, FileSystem, Main, Workspace }) => {
+  const tmpDir = await FileSystem.getTmpDir()
+  const uri = `${tmpDir}/test.js`
+  const text =
+    'class Example { get value() { return 1; 2 } } new Example().value'
+  await FileSystem.writeFile(uri, text)
+  await Workspace.setPath(tmpDir)
+  await Main.openUri(uri)
+
+  const { result: diagnostics } = (await Command.executeExtensionCommand(
+    'eslint.lint',
+    { text, uri },
+  )) as { result: readonly { source: string; type: string }[] }
+  const actualDiagnostics = diagnostics.map(({ source, type }) => ({
+    source,
+    type,
+  }))
+  if (
+    JSON.stringify(actualDiagnostics) !== JSON.stringify(expectedDiagnostics)
+  ) {
+    throw new Error(`Unexpected diagnostics: ${JSON.stringify(diagnostics)}`)
+  }
+}
