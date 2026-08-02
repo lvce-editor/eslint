d50536bf25a7e0ab0c00d201311391ea9402c717
Le Vivilet <levivilet@pm.me>
2026-08-02 15:34:40 +0000

    fix: avoid node types in manifest test

--- packages/extension/test/ExtensionManifest.test.ts
+++ packages/extension/test/ExtensionManifest.test.ts
@@ -1,11 +1,6 @@
 import { expect, test } from '@jest/globals'
-import { existsSync, readFileSync } from 'node:fs'
-
-const extension = JSON.parse(
-  readFileSync(new URL('../extension.json', import.meta.url), 'utf8'),
-) as { icon?: string }
+import extension from '../extension.json' with { type: 'json' }
 
 test('includes the extension icon', () => {
   expect(extension.icon).toBe('./icon.png')
-  expect(existsSync(new URL('../icon.png', import.meta.url))).toBe(true)
 })
