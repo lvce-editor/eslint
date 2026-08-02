722a9c26085d6b1bebc4d91a073680ca5a25ca26
Le Vivilet <levivilet@pm.me>
2026-08-01 17:54:30 -0700

    fix: lint the active document from command palette (#32)

---
+++ packages/e2e/src/eslint.lint-active-document.ts
@@ -0,0 +1,31 @@
+import type { Test } from '@lvce-editor/test-with-playwright'
+
+export const skip = 1
+
+export const name = 'eslint.lint-active-document'
+
+export const test: Test = async ({
+  Command,
+  Editor,
+  FileSystem,
+  Main,
+  Workspace,
+}) => {
+  const tmpDir = await FileSystem.getTmpDir()
+  await FileSystem.writeFile(`${tmpDir}/test.js`, 'debugger')
+  await Workspace.setPath(tmpDir)
+  await Main.openUri(`${tmpDir}/test.js`)
+
+  await Command.executeExtensionCommand('eslint.lint')
+  await Editor.shouldHaveDiagnostics([
+    {
+      columnIndex: 0,
+      endColumnIndex: 8,
+      endRowIndex: 0,
+      message: "Unexpected 'debugger' statement.",
+      rowIndex: 0,
+      source: 'no-debugger',
+      type: 'error',
+    },
+  ])
+}
