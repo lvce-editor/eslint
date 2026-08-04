73f6e583bcc29330bb325d74b3c7cff0049a0918
Le Vivilet <levivilet@pm.me>
2026-08-04 02:56:28 -0700

    Move ESLint icon to media folder (#54)

--- packages/extension/test/ExtensionManifest.test.ts
+++ packages/extension/test/ExtensionManifest.test.ts
@@ -2,7 +2,7 @@ import { expect, test } from '@jest/globals'
 import extension from '../extension.json' with { type: 'json' }
 
 test('includes the extension icon', () => {
-  expect(extension.icon).toBe('./icon.png')
+  expect(extension.icon).toBe('./media/icon.png')
 })
 
 test('contributes the ESLint output channel', () => {
