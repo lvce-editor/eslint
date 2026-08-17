241d193f168badf2401a0e8fc7bb815ad33b3f0e
Le Vivilet <levivilet@pm.me>
2026-08-17 11:02:42 +0000

    fix: support commonjs default imports marked as esm

--- packages/e2e/fixtures/large-typescript-workspace/eslint.config.js
+++ packages/e2e/fixtures/large-typescript-workspace/eslint.config.js
@@ -1,6 +1,7 @@
 import fs from 'node:fs'
 import path from 'node:path'
 import typescriptEslint from 'typescript-eslint'
+import commonJsPlugin from './commonjs-plugin.cjs'
 import { rules } from './.eslint-plugin-local/index.ts'
 
 fs.readFileSync(path.join(import.meta.dirname, '.eslint-ignore'), 'utf8')
@@ -15,6 +16,7 @@ export default [
       },
     },
     plugins: {
+      commonjs: { rules: commonJsPlugin.rules },
       local: { rules },
     },
     rules: {
